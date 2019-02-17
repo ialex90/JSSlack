7b98ace86bdbec0c7525c53627d67b006bbc7030
Alejandro <alejandrofpo@gmail.com>
2019-02-17 01:20:28 +0100

    .

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
+})
+export class AppComponent {
+  link =
+    "https://join.slack.com/t/aprendiendo-js/shared_invite/enQtNTUyOTg2MjU0OTYzLWIyMDRjNmVkZGE5ZDBjZGI1ZTczNjRjZDZkMWI4ZTllZDg0ZjhlOTcyNDRhNzhhYTdhYjNmYTgxMWJiOGZlY2Q";
+}
